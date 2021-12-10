d97fa5d6cb590fac2b43f8916aab2221fbaf7681
Wellington Willers <wellington.willers@gmail.com>
2021-12-09 23:19:51 -0300

    deploy

---
+++ precache-manifest.0db6ab0f7126837e8d205e0ffbdfa28e.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5b9e1080773f7f5f355",
+    "url": "/2021/clienteespecial/css/app.96873b8e.css"
+  },
+  {
+    "revision": "c96b31374534ee735c60",
+    "url": "/2021/clienteespecial/css/chunk-vendors.9f00a941.css"
+  },
+  {
+    "revision": "aba97805ec56b39c761b11cc08471bfc",
+    "url": "/2021/clienteespecial/favicon.png"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/2021/clienteespecial/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/2021/clienteespecial/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "41d84a3be4abff87a9dd03ac7e1ebe81",
+    "url": "/2021/clienteespecial/img/blob.svg"
+  },
+  {
+    "revision": "b8fd77054df66b541e7e83ef09dfc5db",
+    "url": "/2021/clienteespecial/img/capablur1.png"
+  },
+  {
+    "revision": "013a2df3d0ee561495c63d111ef5adc4",
+    "url": "/2021/clienteespecial/img/capablur2.png"
+  },
+  {
+    "revision": "4f98cab42ce0ed1dc5d7ce3557e82042",
+    "url": "/2021/clienteespecial/img/capafidem.png"
+  },
+  {
+    "revision": "e3aae5ca75755d69c0073328f901cbee",
+    "url": "/2021/clienteespecial/img/capagameplay.png"
+  },
+  {
+    "revision": "2049e7b05da6d5c6ac5fd22c2d55d6a1",
+    "url": "/2021/clienteespecial/img/capagorestaurant.png"
+  },
+  {
+    "revision": "a746dfaff99220c68db58124f89abb94",
+    "url": "/2021/clienteespecial/img/capahappy.png"
+  },
+  {
+    "revision": "1fa5865b04f59e2767e32dda731e405b",
+    "url": "/2021/clienteespecial/img/capaignews.png"
+  },
+  {
+    "revision": "5b198cb8005d8fdb2d74e88fd0db1d55",
+    "url": "/2021/clienteespecial/img/capaletmeask.png"
+  },
+  {
+    "revision": "41a0fd680ce5e397a3215f362da3f35e",
+    "url": "/2021/clienteespecial/img/capaproffy.png"
+  },
+  {
+    "revision": "d09e668432ece802ff151df3ed684dca",
+    "url": "/2021/clienteespecial/img/caparekpay.png"
+  },
+  {
+    "revision": "c148cd009ae1a2fe7a39ea23d238affc",
+    "url": "/2021/clienteespecial/img/caparocketshoes.png"
+  },
+  {
+    "revision": "5e0fbab97aa5871203612f4c66a20210",
+    "url": "/2021/clienteespecial/img/capaspacetraveling.png"
+  },
+  {
+    "revision": "ecfeee58193948398d8095349c98b00b",
+    "url": "/2021/clienteespecial/img/capatesla.png"
+  },
+  {
+    "revision": "138f45676f9e4028eae07a491df4d2cc",
+    "url": "/2021/clienteespecial/img/capawatchme.png"
+  },
+  {
+    "revision": "3f5c2c4e03a028d9299534bb1c8f57dd",
+    "url": "/2021/clienteespecial/img/dtmoney.png"
+  },
+  {
+    "revision": "3ca08123fb268af597a6c04ae0a0c236",
+    "url": "/2021/clienteespecial/img/git.gif"
+  },
+  {
+    "revision": "cc2270bd478699e42a1bf1b0a8efa813",
+    "url": "/2021/clienteespecial/img/logo.cc2270bd.png"
+  },
+  {
+    "revision": "704c87ba3a0aea16ba2b8f514debfebb",
+    "url": "/2021/clienteespecial/index.html"
+  },
+  {
+    "revision": "f5b9e1080773f7f5f355",
+    "url": "/2021/clienteespecial/js/app.83d0ae64.js"
+  },
+  {
+    "revision": "c96b31374534ee735c60",
+    "url": "/2021/clienteespecial/js/chunk-vendors.58a1a7c7.js"
+  },
+  {
+    "revision": "7c4cc9ba92e8692842da4520edb700c8",
+    "url": "/2021/clienteespecial/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/2021/clienteespecial/robots.txt"
+  }
+]);
